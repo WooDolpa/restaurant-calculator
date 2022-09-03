c31b37c91ccb744e60e6d5269fd4475ee7ae311e
jwlee <dannis1124@naver.com>
2022-09-03 22:01:01 +0900

    android project commit

---
+++ app/src/main/java/com/example/restaurant/calculator/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.restaurant.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+
+    }
+
+
+    private void init() {}
+
+}
